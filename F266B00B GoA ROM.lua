b43e6f9441545ccf01d917d76db0776da09f7d95
andrewabenoja <codename_geek@hotmail.com>
2024-12-10 09:18:16 -0800

    Completion Mark Test 2

--- F266B00B GoA ROM.lua
+++ F266B00B GoA ROM.lua
@@ -17,6 +17,7 @@ CheckCount = 0
 lastInput1 = 0
 lastInput2 = 0
 lastRoom = 0
+lastWorld = 0
 end
 
 function GetVersion() --Define anchor addresses
@@ -429,59 +430,59 @@ if Place == 0x000F then
 	local WarpDoor = false
 	if Door == 0x0C then --The World that Never Was
 		WarpDoor = 0x15
-		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 0 and ReadByte(Save+0x35C1) > 0 then
+		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 1 and ReadByte(Save+0x35C1) > 0 then
 			WriteByte(Save+0x35C1,ReadByte(Save+0x35C1)+1)
-			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-1)
+			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
 			print("Converted Completion Mark to Visit Unlock - Way to the Dawn")
 		end
 	elseif Door == 0x03 then --Land of Dragons
 		WarpDoor = 0x16
-		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 0 then
+		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 1 then
 			WriteByte(Save+0x35AF,ReadByte(Save+0x35AF)+1)
-			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-1)
+			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
 			print("Converted Completion Mark to Visit Unlock - Sword of the Ancestors")
 		end
 	elseif Door == 0x04 then --Beast's Castle
 		WarpDoor = 0x17
-		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 0 then
+		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 1 then
 			WriteByte(Save+0x35B3,ReadByte(Save+0x35B3)+1)
-			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-1)
+			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
 			print("Converted Completion Mark to Visit Unlock - Beast's Claw")
 		end
 	elseif Door == 0x09 then --Halloween Town	
 		WarpDoor = 0x18
-		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 0 then
+		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 1 then
 			WriteByte(Save+0x35B4,ReadByte(Save+0x35B4)+1)
-			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-1)
+			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
 			print("Converted Completion Mark to Visit Unlock - Bone Fist")
 		end
 	elseif Door == 0x0A then --Agrabah
 		WarpDoor = 0x19
-		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 0 then
+		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 1 then
 			WriteByte(Save+0x35C0,ReadByte(Save+0x35C0)+1)
-			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-1)
+			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
 			print("Converted Completion Mark to Visit Unlock - Scimitar")
 		end
 	elseif Door == 0x05 then --Olympus Coliseum
 		WarpDoor = 0x1A
-		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 0 then
+		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 1 then
 			WriteByte(Save+0x35AE,ReadByte(Save+0x35AE)+1)
-			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-1)
+			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
 			print("Converted Completion Mark to Visit Unlock - Battlefields of War")
 		end
 	elseif Door == 0x0B then --Pride Lands
 		WarpDoor = 0x1B
-		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 0 then
+		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 1 then
 			WriteByte(Save+0x35B5,ReadByte(Save+0x35B5)+1)
-			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-1)
+			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
 			print("Converted Completion Mark to Visit Unlock - Proud Fang")
 		end
 	elseif Door == 0x01 then --Twilight Town
 		if ReadByte(Save+0x1CFF) == 8 then --Twilight Town
 			WarpDoor = 0x1C
-			if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 0 then
+			if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 1 then
 				WriteByte(Save+0x3649,ReadByte(Save+0x3649)+1)
-				WriteByte(Save+0x363D,ReadByte(Save+0x363D)-1)
+				WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
 				print("Converted Completion Mark to Visit Unlock - Ice Cream")
 			end
 		elseif ReadByte(Save+0x1CFF) == 13 then --Simulated Twilight Town
@@ -489,23 +490,23 @@ if Place == 0x000F then
 		end
 	elseif Door == 0x02 then --Hollow Bastion
 		WarpDoor = 0x1D
-		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 0 then
+		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 1 then
 			WriteByte(Save+0x3643,ReadByte(Save+0x3643)+1)
-			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-1)
+			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
 			print("Converted Completion Mark to Visit Unlock - Membership Card")
 		end
 	elseif Door == 0x08 then --Port Royal
 		WarpDoor = 0x1E
-		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 0 then
+		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 1 then
 			WriteByte(Save+0x35B6,ReadByte(Save+0x35B6)+1)
-			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-1)
+			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
 			print("Converted Completion Mark to Visit Unlock - Skill and Crossbones")
 		end
 	elseif Door == 0x06 then --Disney Castle
 		WarpDoor = 0x1F
-		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 0 then
+		if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 1 then
 			WriteByte(Save+0x365D,ReadByte(Save+0x365D)+1)
-			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-1)
+			WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
 			print("Converted Completion Mark to Visit Unlock - Royal Summons")
 		end
 	elseif Door == 0x07 then --Atlantica
@@ -515,14 +516,21 @@ if Place == 0x000F then
 		Warp(0x04,0x1A,WarpDoor)
 	end
 end
-if Room == 26 and (lastRoom == 0 or lastRoom == 5 or lastRoom == 8) then
-	if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 0 then
+if World == 4 and (lastWorld == 17) then -- Space Paranoids
+	if lastInput2 == 2048 and lastInput1 == 0 and ReadByte(Save+0x363D) > 1 then
 		WriteByte(Save+0x35C2,ReadByte(Save+0x35C2)+1)
-		WriteByte(Save+0x363D,ReadByte(Save+0x363D)-1)
+		WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
 		print("Converted Completion Mark to Visit Unlock - Identity Disk")
+	end
+elseif World == 4 and (lastWorld == 9) then -- 100 Acre Wood
+	if (lastInput2 == 33557504 or lastInput1 == 0) and ReadByte(Save+0x363D) > 1 then
+		WriteByte(Save+0x3598,ReadByte(Save+0x3598)+1)
+		WriteByte(Save+0x363D,ReadByte(Save+0x363D)-2)
+		print("Converted Completion Mark to Visit Unlock - Torn Pages")
 	end
 end
 lastRoom = Room
+lastWorld = World
 --Visit Locks
 if true then
 	--Namine's Sketches
